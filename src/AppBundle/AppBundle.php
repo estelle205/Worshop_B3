4eef30afaf2c86dea9aa3c26365ad1e875da3487
Unknown <aubestelle205@gmail.com>
2018-04-10 17:00:25 +0200

    Add du Vendor FOSUserBundle

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -6,13 +6,20 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use FOS\UserBundle\Model\User as FosUser;
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
 class AppBundle extends Bundle
 {
 
 
     /**
-     * AppBundle constructor.
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\Genrerated(strategy="AUTO")
      */
+    protected $id;
     public function __construct()
     {
         parent:: __construct();
