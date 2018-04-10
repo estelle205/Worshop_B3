4a52035c7cffcb8079f1b82ec3e909b7695a855c
Unknown <aubestelle205@gmail.com>
2018-04-10 17:16:45 +0200

    Add AppBundle.php

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,27 +1,9 @@
 <?php
 
-namespace AppBundle\Entity;
+namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use FOS\UserBundle\Model\User as FosUser;
-use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="fos_user")
- */
 class AppBundle extends Bundle
 {
-
-
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\Genrerated(strategy="AUTO")
-     */
-    protected $id;
-    public function __construct()
-    {
-        parent:: __construct();
-    }
 }
